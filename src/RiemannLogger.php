7d76c366c6cfe030a3fea03f2af7f739f9953eca
anho <me@anho.github.com>
2016-05-12 09:29:27 +0200

    use riemann-logger library and deprecate own RiemannLogger class

--- src/RiemannLogger.php
+++ src/RiemannLogger.php
@@ -2,89 +2,11 @@
 
 namespace Trademachines\Bundle\RiemannLoggingBundle;
 
-use Trademachines\Riemann\Client as RiemannClient;
+use Trademachines\RiemannLogger\RiemannLogger as BaseLogger;
 
-class RiemannLogger
+/**
+ * @deprecated Use the logger or interface for the riemann-logger library.
+ */
+class RiemannLogger extends BaseLogger
 {
-    /** @var RiemannClient */
-    protected $riemannClient;
-
-    /** @var string */
-    protected $name;
-
-    /** @var string */
-    protected $identAttribute;
-
-    /**
-     * RiemannLogger constructor.
-     *
-     * @param RiemannClient $riemannClient
-     * @param string|null   $name
-     */
-    public function __construct(RiemannClient $riemannClient, $name = null, $identAttribute = null)
-    {
-        $this->riemannClient  = $riemannClient;
-        $this->name           = $name;
-        $this->identAttribute = $identAttribute;
-    }
-
-    /**
-     * @param array $data
-     * @param array $attributes
-     */
-    public function log(array $data, array $attributes = [])
-    {
-        $eventData = $this->getRiemannEventData($data, $attributes);
-        $this->riemannClient->sendEvent($eventData);
-    }
-
-    protected function getRiemannEventData(array $data, array $attributes)
-    {
-        $data               = $this->enrichData($data);
-        $data['attributes'] = $this->asRiemannAttributes($attributes);
-
-        return $data;
-    }
-
-    protected function enrichData(array $data)
-    {
-        if (!isset($data['host'])) {
-            $data['host'] = gethostname();
-        }
-
-        if ($service = $this->getService($data)) {
-            $data['service'] = $service;
-        }
-
-        return $data;
-    }
-
-    protected function asRiemannAttributes(array $data)
-    {
-        $attributes = [];
-        foreach ($data as $key => $value) {
-            $attributes[] = [
-                'key'   => $key,
-                'value' => $value,
-            ];
-        }
-
-        if ($this->name && $this->identAttribute) {
-            $attributes[] = [
-                'key'   => $this->identAttribute,
-                'value' => $this->name,
-            ];
-        }
-
-        return $attributes;
-    }
-
-    private function getService(array $data)
-    {
-        if (!$this->name || isset($data['service'])) {
-            return null;
-        }
-
-        return $this->name;
-    }
 }
